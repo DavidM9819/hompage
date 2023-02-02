d751d9e066daa91b3df6a335c881eb489b045272
Dawid Maziarz <david9819@onet.eu>
2023-02-02 17:27:47 +0100

    add function to js files

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
-console.log("Cześć!");
-let button = document.querySelector(".button");
-let js_headerMain = document.querySelector(".headerMain");
-let js_headerMain__Dark = document.querySelector(".headerMain__Dark");
-let js_button__headerText = document.querySelector(".button__headerText");
+{
+  const welcome = () => {
+    console.log("Cześć!");
+  };
 
-button.addEventListener("click", () => {
-  js_headerMain.classList.toggle("headerMain__Dark");
+  const backgroundToggle = () => {
+    let js_headerMain = document.querySelector(".headerMain");
+    js_headerMain.classList.toggle("headerMain__Dark");
+    let js_button__headerText = document.querySelector(".button__headerText");
+    let js_headerMain__Dark = document.querySelector(".headerMain__Dark");
+    if (js_headerMain.classList.contains("headerMain__Dark")) {
+      js_button__headerText.innerText = "Ciemny";
+    } else {
+      js_button__headerText.innerText = "Jasny";
+    }
+  };
 
-  if (js_headerMain.classList.contains("headerMain__Dark")) {
-    js_button__headerText.innerText = "Ciemny";
-  } else {
-    js_button__headerText.innerText = "Jasny";
-  }
-});
+  const init = () => {
+    let button = document.querySelector(".button");
+    button.addEventListener("click", backgroundToggle);
+    welcome();
+  };
+  init();
+}
