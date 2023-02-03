a4c244421e6e036428dbac209d08dfd133b4fc75
Dawid Maziarz <david9819@onet.eu>
2023-02-03 15:21:54 +0100

    change 'let' to 'const'  in js file

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@
   };
 
   const backgroundToggle = () => {
-    let js_headerMain = document.querySelector(".headerMain");
+    const js_headerMain = document.querySelector(".headerMain");
     js_headerMain.classList.toggle("headerMain__Dark");
-    let js_button__headerText = document.querySelector(".button__headerText");
-    let js_headerMain__Dark = document.querySelector(".headerMain__Dark");
+    const js_button__headerText = document.querySelector(".button__headerText");
+    const js_headerMain__Dark = document.querySelector(".headerMain__Dark");
     if (js_headerMain.classList.contains("headerMain__Dark")) {
       js_button__headerText.innerText = "Ciemny";
     } else {
@@ -16,7 +16,7 @@
   };
 
   const init = () => {
-    let button = document.querySelector(".button");
+    const button = document.querySelector(".button");
     button.addEventListener("click", backgroundToggle);
     welcome();
   };
